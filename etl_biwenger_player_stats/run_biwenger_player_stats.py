eb4674bc0b7014d94662bc6698fa27503a8e9b20
Jose Parreño Garcia <jose.parreno@skyscanner.net>
2025-05-20 10:27:07 +0200

    Data run jornada 37

--- etl_biwenger_player_stats/run_biwenger_player_stats.py
+++ etl_biwenger_player_stats/run_biwenger_player_stats.py
@@ -2,13 +2,15 @@ import time
 from rich.console import Console
 from etl_biwenger_player_stats.scraper_biwenger_player_stats.player_scraper import scraper
 from etl_biwenger_player_stats.transform_biwenger import transform_players
+from etl_biwenger_player_stats.scraper_biwenger_player_stats.utils import (calculate_season)
+
 
 console = Console()
 
 if __name__ == "__main__":
     start_time = time.time()
-    # console.rule("[bold blue]Extract Step")
-    # scraper()
+    console.rule("[bold blue]Extract Step")
+    scraper(season_tag=calculate_season())
     # scraper(hardcoded_pages=6)
 
     console.rule("[bold yellow]Transform Step")
@@ -16,7 +18,7 @@ if __name__ == "__main__":
 
     # console.rule("[bold green]Load Step")
     # load_to_production()
-    #
+
     total_duration = time.time() - start_time
     console.print(f"[bold white on black]🏁 Full ETL completed in {total_duration:.2f} seconds!")
 
