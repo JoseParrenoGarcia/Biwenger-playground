67af19c5f15f6e9805cb5c0de6801f659cb4e8ab
Jose Parreño Garcia <jose.parreno@skyscanner.net>
2025-06-12 12:44:28 +0200

    Adding celta to config

--- scraper_news_agent/config.py
+++ scraper_news_agent/config.py
@@ -1,10 +1,23 @@
 TEAM_NEWS_SOURCES = {
-    "Valencia": [
-        "https://www.superdeporte.es/valencia-cf/",
-        "https://plazadeportiva.valenciaplaza.com/valenciacf/"
+    "Celta de Vigo": [
+        "https://www.farodevigo.es/celta-de-vigo/",
+        "https://www.lavozdegalicia.es/gradario/",
+        "https://www.estadiodeportivo.com/futbol/celta/",
+        "https://www.eldesmarque.com/futbol/celta-de-vigo/",
+        "https://as.com/noticias/real-club-celta-de-vigo/?omnil=mod_esc",
+        "https://www.marca.com/futbol/celta.html?intcmp=MENUESCU&s_kw=celta",
     ],
     "Real Madrid": [
         "https://www.marca.com/futbol/real-madrid.html?intcmp=MENUESCU&s_kw=realmadrid",
     ],
-    # Add more teams as needed
+    "Valencia": [
+        "https://www.superdeporte.es/valencia-cf/",
+        "https://plazadeportiva.valenciaplaza.com/valenciacf/",
+        "https://www.levante-emv.com/valencia-cf/",
+        "https://www.lasprovincias.es/valenciacf/",
+        "https://www.marca.com/futbol/valencia.html?intcmp=MENUESCU&s_kw=valencia,"
+        "https://as.com/noticias/valencia-cf/?omnil=mod_esc",
+        "https://www.estadiodeportivo.com/futbol/valencia/",
+        "https://www.eldesmarque.com/futbol/valencia-cf/",
+    ],
 }
